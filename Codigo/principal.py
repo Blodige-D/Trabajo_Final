47525769ec5a9372439f5481d1f1dd4cde8209f0
Nicolasrz122 <68756886+Nicolasrz122@users.noreply.github.com>
2021-02-21 21:34:24 -0500

    vamos

--- Codigo/principal.py
+++ Codigo/principal.py
@@ -1,14 +1,52 @@
 import cx_Oracle
 
-conexion = cx_Oracle.connect("system", "1234", "localhost/XE")
-cursor = conexion.cursor()
-cursor.execute('Select * from cliente')
+class connect():
+    def __init__(self):
 
-try:
-    for columnas in cursor:
-        print(columnas)
-        cursor.close()
-        conexion.close()
-except Exception as e:
-    print(str(e))
+        host = "192.168.20.28"
+        user = "Final"
+        passw = "1234"
+        tsname = "xe"
+
+        cx_Oracle.init_oracle_client(lib_dir=r"C:\oraclexe\app\oracle\instantclient_19_6")
+        try:
+            self.conexion = cx_Oracle.connect(user, passw, host+"/"+tsname+"?connect_timeout=30")
+            print("Conexion Establecida!!")
+            
+        except Exception as error:
+            print("No se pudo conectar a la base de datos. Error : " + error)
+        
+    def sentenciaCompuesta(self, sentencia):
+        cursor = self.conexion.cursor()
+        cursor.execute(sentencia)
+        datos = cursor.fetchall()
+        cursor.close
+        return datos
+    
+    def sentenciaSimple(self, sentencia):
+        cursor = self.conexion.cursor()
+        cursor.execute(sentencia)
+        cursor.close
+
+    def close(self):
+        self.conexion.close()
+
+    def commit(self):
+        self.conexion.commit()
+
+
+if __name__ == "__main__":
+    nexo = connect()
+    nexo.sentenciaSimple("insert into CLIENTE values (1083764589, 'Kevin Andres', 'Forero Gusteiro', '3055874345', 'Av 167 #456-1456')")
+    nexo.commit()
+    for fila in nexo.sentenciaCompuesta("select * from CLIENTE"):
+        print(fila)
+       
+    
+    nexo.close()
+        
+
+
+
+
 
